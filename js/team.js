9c8e000e04ba22efd70f46fadfb24f0117f3a183
Matthew Kleiman <bigmickey@gmail.com>
2013-08-14 20:03:05 -0400

    carousel with popover

--- js/team.js
+++ js/team.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
-  $('.carousel').carousel({
-    interval: false
-  });
+//  $('.carousel').carousel({
+//    interval: false
+//  });
+
+
+  var popoverOptions = {
+    placement: 'right',
+    trigger: 'click hover',
+    container: '#teamCarousel',
+    html: true,
+    content: function() {
+      return $('#popover-content-wrapper').children('[data-popover-content-for='+$(this).attr('id')+']').html();
+    }
+  };
+
+  $('.btn-popover').popover(popoverOptions);
 
 });
