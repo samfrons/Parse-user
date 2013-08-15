74b2b872dcb83c10168f81bdef2defc48ffbb67c
Matthew Kleiman <bigmickey@gmail.com>
2013-08-15 14:56:20 -0400

    login changes and using sam's parse account

--- js/app.js
+++ js/app.js
@@ -1,73 +1,64 @@
 $(document).ready(function(){
-//  Parse.initialize("fYOLoSeCKgYHCfvHZ4BKq2xFG4W8eZFgkCMeDb1p", "WylWNwkFQhxFTjmmaOWqhu2v5ABNCHA545TbS0r2");
+  Parse.initialize("fYOLoSeCKgYHCfvHZ4BKq2xFG4W8eZFgkCMeDb1p", "WylWNwkFQhxFTjmmaOWqhu2v5ABNCHA545TbS0r2"); // samfrons@gmail.com Hasbro app
 //  Parse.initialize("r3odn9sQibfbr1DItzNQ5L6Rchum1nzEZdPtqLap", "Rvr8te1R8UUYlH82sy4n0loSxmG7dyYvhbAhGraw");
-  Parse.initialize("GTRtkAfkV09eEC2Olkdxdr4QJcb378XPkxCRCZkP", "K5hYtNZpa1dQIhArbW8iOGosnUkEDt7AQ4vUzkYa"); // Matt's HasbroTest app
+//  Parse.initialize("GTRtkAfkV09eEC2Olkdxdr4QJcb378XPkxCRCZkP", "K5hYtNZpa1dQIhArbW8iOGosnUkEDt7AQ4vUzkYa"); // Matt's HasbroTest app
 
+  // Test for logged in user
   var currentUser = Parse.User.current();
-  if (currentUser) {
-    // OK
-    var url = window.location.pathname;
-    var currentName = url.substring(url.lastIndexOf('/') + 1);
-    if(currentName === "index.html") {
-      window.location = "welcome.html";
-      return;
-    }
-  } else {
-    var url = window.location.pathname;
-    var currentName = url.substring(url.lastIndexOf('/') + 1);
-    if(currentName !== "index.html") {
-      window.location = "index.html";
-      return;
-    }
+  var currentPath = window.location.pathname;
+  var currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+  if (currentUser && currentPath === "index.html") {
+    // user already logged in, redirect to welcome page
+    window.location = "welcome.html";
+  } else if (!currentUser && currentPage !== "index.html") {
+    // user needs to first login
+    window.location = "index.html";
   }
-  /*var TestObject = Parse.Object.extend("TestObject");
-   var testObject = new TestObject();
-   testObject.save({foo: "bar"}, {
-   success: function(object) {
 
-   }*/
-
-  $('#btnlogin').bind('click', function(e){
+  $('#btnlogin').on('click', function(e){
+//  window.submitLogin = function() {
     if(e) e.stopPropagation();
 
-    $(this).button('loading');
-    $('#errormsg_incorrect_username_password').removeClass('show');
-    $('#errormsg_incorrect_username_password').addClass('hide');
+    $('#btnlogin').button('loading');
+    $('#errormsg_incorrect_username_password').removeClass('show').addClass('hide'); // hide error
 
     var username = encodeURIComponent($('#email').val());
     var password = encodeURIComponent($('#password').val());
 
-    if(!username && !password){
-      $('#errormsg_incorrect_username_password').removeClass('hide');
-      $('#errormsg_incorrect_username_password').addClass('show');
-      return;
+    if (!username && !password) { // must supply username & password
+      $('#btnlogin').button('reset');
+      $('#errormsg_incorrect_username_password').removeClass('hide').addClass('show'); // show error
+    } else {
+      Parse.User.logIn(username, password, {
+        success: function(user) {
+          console.log('Login success', username, password, user)
+          // redirect to our welcome page
+          $('#btnlogin').button('reset');
+          $('#errormsg_incorrect_username_password').removeClass('show').addClass('hide'); // hide error
+          window.location = "welcome.html";
+        },
+        error: function(user, error) {
+          console.log('Login error', username, password, user, error)
+          // The login failed. Check error to see why.
+          $('#btnlogin').button('reset');
+          $('#errormsg_incorrect_username_password').removeClass('hide').addClass('show'); // show error
+        }
+      });
     }
-
-    Parse.User.logIn(username, password, {
-      success: function(user) {
-        console.log('Login success', username, password, user)
-        // redirect to our welcome page
-        $('#btnlogin').button('reset');
-        window.location = "welcome.html";
-      },
-      error: function(user, error) {
-        console.log('Login error', username, password, user, error)
-        // The login failed. Check error to see why.
-        $('#errormsg_incorrect_username_password').removeClass('hide');
-        $('#errormsg_incorrect_username_password').addClass('show');
-        $('#btnlogin').button('reset');
-      }
-    });
+//  };
   });
 
+
+  // logout button click
   $('[name="btnlogout"]').bind('click', function(e){
     if(e) e.stopPropagation();
     Parse.User.logOut();
     window.location = "index.html";
   });
 
-  var mq = window.matchMedia("(max-width: 400px)");
 
+  // TODO: need comment for unknown mq stuff
+  var mq = window.matchMedia("(max-width: 400px)");
   if (mq.matches) {
     $('#normalrobot').removeClass('show');
     $('#normalrobot').addClass('hide');
